f5197b123d5c2d6b3bf1a714a4f70ebeae4d6826
Redox <mail@redox.to>
2022-05-26 21:56:13 +0200

    Fix linting errors

--- lua/cfc_entity_stubber/cfc_m9k/cfc9k_scoped_base.lua
+++ lua/cfc_entity_stubber/cfc_m9k/cfc9k_scoped_base.lua
@@ -6,32 +6,32 @@ cfcEntityStubber.registerStub( function()
     function weapon:ScopeIN()
         local owner = self:GetOwner()
 
-        timer.Remove("m9k_resetscope_" .. self.OurIndex)
+        timer.Remove( "m9k_resetscope_" .. self.OurIndex )
         owner:SetFOV( 10, 0.2 )
         self.DrawCrosshair = false
         self:SetNWInt( "ScopeState", 100 )
         owner:DrawViewModel( false )
         if CLIENT then return end
-        owner:EmitSound("weapons/zoom.wav")
+        owner:EmitSound( "weapons/zoom.wav" )
     end
 
     function weapon:ScopeOUT()
         local owner = self:GetOwner()
 
-        timer.Remove("m9k_resetscope_" .. self.OurIndex)
+        timer.Remove( "m9k_resetscope_" .. self.OurIndex )
         owner:SetFOV( 0, 0.1 )
         self.DrawCrosshair = true
         self:SetNWInt( "ScopeState", 0 )
         owner:DrawViewModel( true )
         if CLIENT then return end
-        owner:EmitSound("weapons/zoom.wav")
+        owner:EmitSound( "weapons/zoom.wav" )
     end
 
     function weapon:IronSight()
         local owner = self:GetOwner()
-        local scopeState = self:GetNWInt("ScopeState")
+        local scopeState = self:GetNWInt( "ScopeState" )
 
-        if owner:GetViewEntity() ~= owner or not self:GetNWBool("CanIronSights") then return end
+        if owner:GetViewEntity() ~= owner or not self:GetNWBool( "CanIronSights" ) then return end
 
         if scopeState == 0 then
             owner:DrawViewModel( true )
@@ -66,11 +66,11 @@ cfcEntityStubber.registerStub( function()
 
     function weapon:Reload()
         if SERVER and game.SinglePlayer() then self:CallOnClient( "Reload" ) end
-        timer.Remove("m9k_resetscope_" .. self.OurIndex)
+        timer.Remove( "m9k_resetscope_" .. self.OurIndex )
 
         local owner = self:GetOwner()
 
-        if self:GetNWInt("ScopeState") > 0 then
+        if self:GetNWInt( "ScopeState" ) > 0 then
             owner:SetFOV( 0, 0.1 )
             self.DrawCrosshair = true
             self:SetNWInt( "ScopeState", 0 )
@@ -81,14 +81,14 @@ cfcEntityStubber.registerStub( function()
             self.NextReloadTime = CurTime() + 0.5
         end
 
-        if self.CanReload and self.NextReloadTime < CurTime() and owner:GetAmmoCount(self.Primary.Ammo) >= 1 and self:Clip1() < self:GetMaxClip1() then
+        if self.CanReload and self.NextReloadTime < CurTime() and owner:GetAmmoCount( self.Primary.Ammo ) >= 1 and self:Clip1() < self:GetMaxClip1() then
             self:DefaultReload( ACT_VM_RELOAD )
             owner:SetAnimation( PLAYER_RELOAD )
         end
     end
 
     function weapon:SecondaryAttack()
-        if self:GetNWInt("ScopeState") == 99 then
+        if self:GetNWInt( "ScopeState" ) == 99 then
             self:ScopeIN()
         end
     end
