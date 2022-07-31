519db73db81dd8e1d6c2fbb0711597dcb1323a0c
Pierce <pierce@vulpes.dev>
2022-07-31 14:29:34 -0400

    disable scaleSensivityToSpread for all cw guns

---
+++ lua/cfc_entity_stubber/cw/cw_base.lua
@@ -0,0 +1,7 @@
+AddCSLuaFile()
+cfcEntityStubber.registerStub( function()
+    -- https://github.com/Facepunch/garrysmod-issues/issues/5290 this issue makes some guns such as the cw rpg unusable on base branches
+    -- So we just disable the spread sensitivit scaling
+    local weapon = cfcEntityStubber.getWeapon( "cw_base" )
+    weapon.scaleSensitivityToSpread = false
+end )
