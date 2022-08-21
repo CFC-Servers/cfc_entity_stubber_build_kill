0b8870f9bed286f2458d20ef05e7cc080a550d61
Pierce <pierce@vulpes.dev>
2022-08-21 17:09:46 -0400

    stub mouse sensitivity for cw base

--- lua/cfc_entity_stubber/cw/cw_base.lua
+++ lua/cfc_entity_stubber/cw/cw_base.lua
@@ -2,6 +2,14 @@ AddCSLuaFile()
 cfcEntityStubber.registerStub( function()
     -- https://github.com/Facepunch/garrysmod-issues/issues/5290 this issue makes some guns such as the cw rpg unusable on base branches
     -- So we just disable the spread sensitivity scaling
-    local weapon = cfcEntityStubber.getWeapon( "cw_base" )
-    weapon.scaleSensitivityToSpread = false
+    local cwBase = cfcEntityStubber.getWeapon( "cw_base" )
+    cwBase.scaleSensitivityToSpread = false
+
+    cwBase._AdjustMouseSensitivity = cwBase._AdjustMouseSensitivity or cwBase.AdjustMouseSensitivity
+
+    cwBase.AdjustMouseSensitivity = function(self, ...)
+        self.OverallMouseSens = 1
+        
+        return cwBase._AdjustMouseSensitivity(self, ...)
+    end
 end )
