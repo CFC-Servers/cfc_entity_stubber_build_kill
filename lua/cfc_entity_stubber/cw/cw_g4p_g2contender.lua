38f06be7b02c1f18413b06bfb6202ca03020844b
GenericRicochetPlayer <113946528+GenericRicochetPlayer@users.noreply.github.com>
2022-12-02 16:42:41 -0500

    contender reload+force nerf, and deagle buff (#18)
    * Add files via upload
    
    * Add player check
    
    * Update cw_g4p_g2contender.lua
    
    Co-authored-by: Redox <mail@redox.to>

--- lua/cfc_entity_stubber/cw/cw_g4p_g2contender.lua
+++ lua/cfc_entity_stubber/cw/cw_g4p_g2contender.lua
@@ -9,11 +9,11 @@ cfcEntityStubber.registerStub( function()
     weapon.HipSpread = 0.1
     weapon.SpreadPerShot = 0.01
     weapon.ClumpSpread = 0.1
-    weapon.ReloadSpeed = 0.7
+    weapon.ReloadSpeed = 0.65
 
     local IsValid = IsValid
 
-    hook.Add( "EntityTakeDamage", "CW_Stubber_BuffWhateverGunForce", function( _, dmg )
+    hook.Add( "EntityTakeDamage", "CW_Stubber_BuffWhateverGunForce", function( ent, dmg )
         local attacker = dmg:GetAttacker()
         if not IsValid( attacker ) then return end
         if not attacker:IsPlayer() then return end
@@ -23,6 +23,10 @@ cfcEntityStubber.registerStub( function()
 
         if wep:GetClass() ~= "cw_g4p_g2contender" then return end
 
-        dmg:SetDamageForce( dmg:GetDamageForce() * 30 )
+        if IsValid( ent ) and ent:IsPlayer() then
+            dmg:SetDamageForce( dmg:GetDamageForce() * 100 )
+        else
+            dmg:SetDamageForce( dmg:GetDamageForce() * 7 )
+        end
     end )
 end )
