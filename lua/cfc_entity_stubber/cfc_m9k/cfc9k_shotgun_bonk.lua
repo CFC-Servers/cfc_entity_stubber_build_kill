670389041b5378cffb9b2ca5221ef81507ea6cd2
legokidlogan <legokidlogan@gmail.com>
2022-12-12 15:42:21 -0700

    Improve launching

--- lua/cfc_entity_stubber/cfc_m9k/cfc9k_shotgun_bonk.lua
+++ lua/cfc_entity_stubber/cfc_m9k/cfc9k_shotgun_bonk.lua
@@ -1,5 +1,56 @@
 AddCSLuaFile()
 
+local function mathSign( x )
+    if x > 0 then return 1 end
+    if x < 0 then return -1 end
+    return 0
+end
+
+local function enoughToKill( ply, dmgAmount )
+    local health = ply:Health()
+    local armor = ply:Armor()
+
+    -- Note: this currently doesn't check for godmode
+    if dmgAmount >= health + armor then
+        return true
+    end
+
+    return false
+end
+
+local function bonkPlayer( victim, wep, baseForce )
+    local force = baseForce * wep.Bonk.PlayerForceMult
+    local zSign = mathSign( force.z )
+    local zMin = wep.Bonk.PlayerForceMinZ
+    local zMax = wep.Bonk.PlayerForceMaxZ
+
+    -- z-axis ends up being the dominant aspect of what sends a player far or not when they're on the ground
+    force.z = math.Clamp( math.abs( force.z ), zMin, zMax ) * zSign
+
+    victim:SetVelocity( force )
+end
+
+local function bonkVictim( attacker, victim, dmg, wep )
+    if IsValid( victim ) and victim:IsPlayer() then
+        local force = dmg:GetDamageForce()
+
+        -- When both players are on the ground, the force is often downwards, which makes it very weak
+        if attacker:IsOnGround() and victim:IsOnGround() then
+            force.z = math.abs( force.z )
+        end
+
+        -- ETD DamageForce on players only affects their death ragdoll
+        if enoughToKill( victim, dmg:GetDamage() ) then
+            dmg:SetDamageForce( force * wep.Bonk.PlayerForceMultRagdoll )
+        else
+            bonkPlayer( victim, wep, force )
+        end
+    else
+        dmg:SetDamageForce( dmg:GetDamageForce() * wep.Bonk.PropForceMult )
+    end
+end
+
+
 cfcEntityStubber.registerStub( function()
     local weaponClass = "m9k_ithacam37"
     local weapon = cfcEntityStubber.getWeapon( weaponClass )
@@ -14,12 +65,15 @@ cfcEntityStubber.registerStub( function()
     weapon.Primary.KickHorizontal = 5
     weapon.Primary.NumShots = 10
     weapon.Primary.Damage = 2
-    weapon.Primary.Spread = 0.1
-    weapon.Primary.IronAccuracy = 0.07
+    weapon.Primary.Spread = 0.05
+    weapon.Primary.IronAccuracy = 0.035
     weapon.ShellTime = 0.4
 
     weapon.Bonk = weapon.Bonk or {}
-    weapon.Bonk.VictimForceMult = 300
+    weapon.Bonk.PlayerForceMult = 0.9
+        weapon.Bonk.PlayerForceMinZ = 210
+        weapon.Bonk.PlayerForceMaxZ = 400
+    weapon.Bonk.PlayerForceMultRagdoll = 300
     weapon.Bonk.PropForceMult = 7
     weapon.Bonk.SelfForce = 500
 
@@ -50,11 +104,7 @@ cfcEntityStubber.registerStub( function()
 
         if wep:GetClass() ~= weaponClass then return end
 
-        if IsValid( ent ) and ent:IsPlayer() then
-            dmg:SetDamageForce( dmg:GetDamageForce() * wep.Bonk.VictimForceMult )
-        else
-            dmg:SetDamageForce( dmg:GetDamageForce() * wep.Bonk.PropForceMult )
-        end
+        bonkVictim( attacker, ent, dmg, wep )
     end )
 end )
 
