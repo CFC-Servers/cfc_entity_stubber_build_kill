da1fc7c0366282fa9f71c0dc5d3958a9e09a3525
legokidlogan <legokidlogan@gmail.com>
2022-12-12 21:06:53 -0700

    Tweak values and add impact damage

--- lua/cfc_entity_stubber/cfc_m9k/cfc9k_shotgun_bonk.lua
+++ lua/cfc_entity_stubber/cfc_m9k/cfc9k_shotgun_bonk.lua
@@ -1,5 +1,17 @@
 AddCSLuaFile()
 
+local IMPACT_DAMAGE_ENABLED = true
+local IMPACT_DAMAGE_MULT = 10 / 7000
+local IMPACT_DAMAGE_MIN = 10
+local IMPACT_DAMAGE_MAX = 50
+local IMPACT_ACCELERATION_THRESHOLD = 7000
+local IMPACT_START_DELAY = 0.05
+local IMPACT_LIFETIME = 7
+local BONK_GUN_CLASS = "m9k_ithacam37"
+
+local IsValid = IsValid
+
+
 local function mathSign( x )
     if x > 0 then return 1 end
     if x < 0 then return -1 end
@@ -18,7 +30,7 @@ local function enoughToKill( ply, dmgAmount )
     return false
 end
 
-local function bonkPlayer( victim, wep, baseForce )
+local function bonkPlayer( attacker, victim, wep, baseForce )
     local force = baseForce * wep.Bonk.PlayerForceMult
     local zSign = mathSign( force.z )
     local zMin = wep.Bonk.PlayerForceMinZ
@@ -26,8 +38,25 @@ local function bonkPlayer( victim, wep, baseForce )
 
     -- z-axis ends up being the dominant aspect of what sends a player far or not when they're on the ground
     force.z = math.Clamp( math.abs( force.z ), zMin, zMax ) * zSign
-
     victim:SetVelocity( force )
+
+    if not IMPACT_DAMAGE_ENABLED then return end
+
+    timer.Simple( IMPACT_START_DELAY, function()
+        if not IsValid( victim ) then return end
+
+        local bonkInfo = victim.cfc9k_bonkInfo
+
+        if not bonkInfo then
+            bonkInfo = {}
+            victim.cfc9k_bonkInfo = bonkInfo
+        end
+
+        bonkInfo.Attacker = attacker
+        bonkInfo.PrevVel = victim:GetVelocity()
+        bonkInfo.IsBonked = true
+        bonkInfo.ExpireTime = RealTime() + IMPACT_LIFETIME
+    end )
 end
 
 local function bonkVictim( attacker, victim, dmg, wep )
@@ -43,17 +72,67 @@ local function bonkVictim( attacker, victim, dmg, wep )
         if enoughToKill( victim, dmg:GetDamage() ) then
             dmg:SetDamageForce( force * wep.Bonk.PlayerForceMultRagdoll )
         else
-            bonkPlayer( victim, wep, force )
+            bonkPlayer( attacker, victim, wep, force )
         end
     else
         dmg:SetDamageForce( dmg:GetDamageForce() * wep.Bonk.PropForceMult )
     end
 end
 
+local function handleImpact( ply, accel )
+    local bonkInfo = ply.cfc9k_bonkInfo
+
+    bonkInfo.IsBonked = false
+    bonkInfo.PrevVel = nil
+    bonkInfo.Attacker = nil
+
+    local damage = math.Clamp( accel * IMPACT_DAMAGE_MULT, IMPACT_DAMAGE_MIN, IMPACT_DAMAGE_MAX )
+    local attacker = IsValid( bonkInfo.Attacker ) and bonkInfo.Attacker or game.GetWorld()
+    local wep = ply:GetWeapon( BONK_GUN_CLASS )
+
+    if not IsValid( wep ) then
+        wep = attacker
+    end
+
+    ply:TakeDamage( damage, attacker, wep )
+end
+
+local function detectImpact( ply, dt )
+    local bonkInfo = ply.cfc9k_bonkInfo
+    if not bonkInfo or not bonkInfo.IsBonked then return end
+
+    local prevVel = bonkInfo.PrevVel
+
+    if not prevVel then
+        bonkInfo.PrevVel = ply:GetVelocity()
+
+        return
+    end
+
+    if RealTime() > bonkInfo.ExpireTime then
+        bonkInfo.IsBonked = false
+        bonkInfo.PrevVel = nil
+        bonkInfo.Attacker = nil
+
+        return
+    end
+
+    local curVel = ply:GetVelocity()
+    local velDiff = curVel - prevVel
+    local accel = velDiff:Length() / dt
+    bonkInfo.PrevVel = curVel
+
+    if accel < IMPACT_ACCELERATION_THRESHOLD then return end
+
+    -- DEBUG
+    bonkInfo.Attacker:ChatPrint( "Impact! " .. math.Round( accel ) .. " " .. math.Round( curVel:Length() ) )
+
+    handleImpact( ply, accel )
+end
+
 
 cfcEntityStubber.registerStub( function()
-    local weaponClass = "m9k_ithacam37"
-    local weapon = cfcEntityStubber.getWeapon( weaponClass )
+    local weapon = cfcEntityStubber.getWeapon( BONK_GUN_CLASS )
 
     weapon.Purpose = ""
     weapon.CFC_Category = "Shotgun:Bonk"
@@ -65,20 +144,18 @@ cfcEntityStubber.registerStub( function()
     weapon.Primary.KickHorizontal = 5
     weapon.Primary.NumShots = 10
     weapon.Primary.Damage = 2
-    weapon.Primary.Spread = 0.09
-    weapon.Primary.IronAccuracy = 0.07
+    weapon.Primary.Spread = 0.1
+    weapon.Primary.IronAccuracy = 0.08
     weapon.ShellTime = 0.4
 
     weapon.Bonk = weapon.Bonk or {}
-    weapon.Bonk.PlayerForceMult = 0.9
+    weapon.Bonk.PlayerForceMult = 0.7
         weapon.Bonk.PlayerForceMinZ = 210
         weapon.Bonk.PlayerForceMaxZ = 400
     weapon.Bonk.PlayerForceMultRagdoll = 300
-    weapon.Bonk.PropForceMult = 7
-    weapon.Bonk.SelfForce = 500
+    weapon.Bonk.PropForceMult = 15
+    weapon.Bonk.SelfForce = 600
 
-
-    local IsValid = IsValid
 
     weapon._ShootBullet = weapon.ShootBullet or cfcEntityStubber.getWeapon( "bobs_gun_base" ).ShootBullet
     weapon.ShootBullet = function( self, damage, recoil, numBullets, spread )
@@ -102,9 +179,23 @@ cfcEntityStubber.registerStub( function()
         local wep = attacker:GetActiveWeapon()
         if not IsValid( wep ) then return end
 
-        if wep:GetClass() ~= weaponClass then return end
+        if wep:GetClass() ~= BONK_GUN_CLASS then return end
 
         bonkVictim( attacker, ent, dmg, wep )
+    end )
+
+
+    if not IMPACT_DAMAGE_ENABLED then return end
+
+    hook.Add( "Think", "M9K_Stubber_BonkGun_DetectImpact", function()
+        local dt = FrameTime()
+        local plys = player.GetAll()
+
+        for i = 1, #plys do
+            local ply = plys[i]
+
+            detectImpact( ply, dt )
+        end
     end )
 end )
 
