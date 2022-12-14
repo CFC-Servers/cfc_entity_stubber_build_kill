9ac2f2de70e5f1145c9d049296bbd50ee64b4f2f
GenericRicochetPlayer <113946528+GenericRicochetPlayer@users.noreply.github.com>
2022-12-14 03:05:31 -0500

    Add files via upload

---
+++ lua/cfc_entity_stubber/cw/cw_g4p_mp412_rex.lua
@@ -0,0 +1,18 @@
+AddCSLuaFile()
+
+cfcEntityStubber.registerStub( function()
+    local weapon = cfcEntityStubber.getWeapon( "cw_g4p_mp412_rex" )
+    weapon.CanRicochet = true
+    weapon.CanPenetrate = false
+    weapon.ReloadSpeed = 1.1
+    weapon.VelocitySensitivity = 0
+    weapon.SpeedDec = 0
+    weapon.HipSpread = 0.001
+    weapon.AimSpread = 0.0005
+    weapon.SpreadPerShot = 0
+    weapon.Damage = 80
+    weapon.SpeedDec = 0
+    weapon.HolsterUnderwater = false
+    weapon.FireDelay = 0.595
+    weapon.NoRicochet = {[MAT_FLESH] = true, [MAT_ANTLION] = false, [MAT_BLOODYFLESH] = false, [MAT_DIRT] = false, [MAT_SAND] = false, [MAT_GLASS] = false, [MAT_ALIENFLESH] = false, [MAT_GRASS] = false}
+end)
