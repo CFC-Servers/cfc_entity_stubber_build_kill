3ab45980064f20d53043923efbdc7d72fe25cbc6
legokidlogan <legokidlogan@gmail.com>
2022-12-18 12:09:00 -0700

    Tweak values

--- lua/cfc_entity_stubber/cfc_m9k/cfc9k_shotgun_bonk.lua
+++ lua/cfc_entity_stubber/cfc_m9k/cfc9k_shotgun_bonk.lua
@@ -236,9 +236,9 @@ cfcEntityStubber.registerStub( function()
     weapon.ShellTime = 0.7
 
     weapon.Bonk = weapon.Bonk or {}
-    weapon.Bonk.PlayerForce = 820 / 0.7 -- Soft-maximum launch strength for when all bullets hit, assuming no special hitgroups (e.g. only hit the chest)
+    weapon.Bonk.PlayerForce = 800 / 0.7 -- Soft-maximum launch strength for when all bullets hit, assuming no special hitgroups (e.g. only hit the chest)
         weapon.Bonk.PlayerForceMultMax = 0.7 -- Damage mult (normal is 1) cannot exceed this value (otherwise could have massive launches from M9K damage spread, headshots, etc.)
-        weapon.Bonk.PlayerForceComboMult = 1.7 -- Multiplies against force strength if the victim is currently in a bonk state
+        weapon.Bonk.PlayerForceComboMult = 1.5 -- Multiplies against force strength if the victim is currently in a bonk state
         weapon.Bonk.PlayerForceGroundZMult = 0.9 -- Makes ground launches be more vertical, proportionally
         weapon.Bonk.PlayerForceGroundZAdd = 0.25 -- Makes ground launches be more vertical, additively
         weapon.Bonk.PlayerForceGroundZMin = 250 -- Minimim z-component of launch force when on the ground. Gmod keeps players grounded unless the the z-vel is ~248.13 or above
