fd942844cceaf0ac75a2ad6275d4efaae2fe0922
GenericRicochetPlayer <113946528+GenericRicochetPlayer@users.noreply.github.com>
2023-03-09 19:50:04 -0500

    Add files via upload (#32)

---
+++ lua/cfc_entity_stubber/cw/cw_m3super90.lua
@@ -0,0 +1,12 @@
+AddCSLuaFile()
+
+cfcEntityStubber.registerStub( function()
+    local weapon = cfcEntityStubber.getWeapon( "cw_m3super90" )
+    weapon.Damage = 15
+    weapon.VelocitySensitivity = 0.8
+    weapon.HipSpread = 0.06
+    weapon.AimSpread = 0.025
+    weapon.ClumpSpread = 0.04
+    weapon.recoil = 4
+    weapon.FireDelay = 0.8
+end )
---
+++ lua/cfc_entity_stubber/cw/cw_xm1014_official.lua
@@ -0,0 +1,11 @@
+AddCSLuaFile()
+
+cfcEntityStubber.registerStub( function()
+    local weapon = cfcEntityStubber.getWeapon( "cw_xm1014_official" )
+    weapon.VelocitySensitivity = 1.5
+    weapon.FireDelay = 0.4
+    weapon.Damage = 10
+    weapon.HipSpread = 0.045
+    weapon.AimSpread = 0.035
+    weapon.SpreadPerShot = 0.02
+end )
