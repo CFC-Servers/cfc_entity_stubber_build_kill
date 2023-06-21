a8cacf005901ebbbabce66a9e17579b7a49e55ea
NullLikesVoid <82908498+NullLikesNothing@users.noreply.github.com>
2023-06-21 18:03:14 -0500

    Actually fix the logic, first commit was an oversight

--- lua/cfc_entity_stubber/cfc_m9k/cfc9k_bonk.lua
+++ lua/cfc_entity_stubber/cfc_m9k/cfc9k_bonk.lua
@@ -326,7 +326,8 @@ hook.Add( "PostEntityTakeDamage", "M9K_Stubber_Bonk_YeetVictim", function( victi
 
     local attacker = dmg:GetAttacker()
     if not IsValid( attacker ) then return end
-    if not attacker:IsPlayer() or not victim:IsNPC() then return end
+    if not attacker:IsPlayer() then return end
+    if victim:IsNPC() then return end
 
     if dmg:GetInflictor() ~= attacker then return end -- Prevent turrets and etc from bonking.
 
