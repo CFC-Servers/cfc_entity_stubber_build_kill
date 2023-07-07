de46d8d265c4c1f41e5ae1767de5deb4358597aa
Decline <Chaselikescandy@gmail.com>
2023-07-07 08:18:18 -0500

    Apply suggestions from Brandon's code review
    Co-authored-by: Brandon Sturgeon <brandon@brandonsturgeon.com>

--- lua/cfc_entity_stubber/cw_attachment/am_flechetterounds.lua
+++ lua/cfc_entity_stubber/cw_attachment/am_flechetterounds.lua
@@ -3,7 +3,13 @@ AddCSLuaFile()
 cfcEntityStubber.registerStub( function()
     local attachment = cfcEntityStubber.getAttachment( "am_flechetterounds" )
 
-    function attachment:attachFunc() end
-    function attachment:detachFunc() end
+    function attachment:attachFunc()
+        self.Shots = 20
+    end
+
+    function attachment:detachFunc()
+        self.Shots = self.Shots_Orig
+    end
+
     cfcEntityStubber.applyAttachmentChange( attachment )
 end )
--- lua/cfc_entity_stubber/cw_attachment/am_slugrounds.lua
+++ lua/cfc_entity_stubber/cw_attachment/am_slugrounds.lua
@@ -7,7 +7,15 @@ cfcEntityStubber.registerStub( function()
         DamageMult = 7.5,
         AimSpreadMult = 1.3
     }
-    function attachment:attachFunc() end
-    function attachment:detachFunc() end
+    function attachment:attachFunc()
+        self.Shots = 1
+        self.ClumpSpread = nil
+    end
+
+    function attachment:detachFunc()
+        self.Shots = self.Shots_Orig
+        self.ClumpSpread = self.ClumpSpread_Orig
+    end
+
     cfcEntityStubber.applyAttachmentChange( attachment )
 end )
