306d45184ba10db6b3c8248a553c47ef9755093a
GenericRicochetPlayer <113946528+GenericRicochetPlayer@users.noreply.github.com>
2023-07-18 04:33:21 -0400

    Add files via upload

--- lua/cfc_entity_stubber/cw/cw_ragingbull.lua
+++ lua/cfc_entity_stubber/cw/cw_ragingbull.lua
@@ -2,10 +2,11 @@ AddCSLuaFile()
 
 cfcEntityStubber.registerStub( function()
     local weapon = cfcEntityStubber.getWeapon( "cw_ragingbull" )
-    weapon.HipSpread = 0.009
-    weapon.AimSpread = 0.009
-    weapon.MaxSpreadInc = 0.03
+    weapon.HipSpread = 0.005
+    weapon.AimSpread = 0.005
+    weapon.MaxSpreadInc = 0.01
     weapon.VelocitySensitivity = 0
+    weapon.SpreadPerShot = 0.01
     weapon.Damage = 70
     weapon.ACF_DamageMult = 6
 end )
