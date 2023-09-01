00ab72ff8eba331c4dbd53a4cd71707de7292979
GenericRicochetPlayer <113946528+GenericRicochetPlayer@users.noreply.github.com>
2023-09-01 14:27:41 -0400

    Add files via upload

--- lua/cfc_entity_stubber/cw/cw_m1911.lua
+++ lua/cfc_entity_stubber/cw/cw_m1911.lua
@@ -3,6 +3,7 @@ AddCSLuaFile()
 cfcEntityStubber.registerStub( function()
     local weapon = cfcEntityStubber.getWeapon( "cw_m1911" )
     weapon.AimSpread = 0.005
+    weapon.HipSpread = 0.02
     weapon.SpreadPerShot = 0.0005
     weapon.Damage = 35
     weapon.ReloadSpeed = 1.5
