2f1f8f6704f7a03852b557d0d5c0ec256cbc6cdb
Brandon Sturgeon <brandon@brandonsturgeon.com>
2023-10-15 14:38:46 -0700

    Base m9k

--- lua/autorun/sh_entity_stubber_init.lua
+++ lua/autorun/sh_entity_stubber_init.lua
@@ -1,2 +1,44 @@
 AddCSLuaFile( "cfc_entity_stubber/main_loader.lua" )
 include( "cfc_entity_stubber/main_loader.lua" )
+
+-- Disable bad sweps
+do
+    local badClasses = {
+        m9k_davy_crockett = true,
+        m9k_ex41 = true,
+        m9k_ied_detonator = true,
+        m9k_m79gl = true,
+        m9k_m202 = true,
+        m9k_matador = true,
+        m9k_milkormgl = true,
+        m9k_nerve_gas = true,
+        m9k_orbital_strike = true,
+        m9k_rpg7 = true,
+    }
+
+    hook.Add( "PreRegisterSWEP", "CFC_EntityStubber_DisableM9kSWEPS", function( _, class )
+        if badClasses[class] then return false end
+    end )
+end
+
+-- Disable bad sents
+do
+    local badClasses = {
+        m9k_davy_crockett_explo = true,
+        m9k_gdcwa_matador_90mm = true,
+        m9k_gdcwa_rpg_heat = true,
+        m9k_launched_davycrockett = true,
+        m9k_launched_ex41 = true,
+        m9k_launched_m79 = true,
+        m9k_m202_rocket = true,
+        m9k_nervegasnade = true,
+        m9k_orbital_cannon = true,
+        m9k_poison_parent = true,
+        m9k_released_poison = true,
+        m9k_sent_nuke_radiation = true,
+    }
+
+    hook.Add( "PreRegisterSENT", "CFC_EntityStubber_DisableM9kSENTs", function( _, class )
+        if badClasses[class] then return false end
+    end )
+end
--- lua/cfc_entity_stubber/stubbers/m9k_stubber.lua
+++ lua/cfc_entity_stubber/stubbers/m9k_stubber.lua
@@ -1,22 +1,2 @@
 AddCSLuaFile()
-
-local function stubM9kBase()
-    local base = cfcEntityStubber.getWeapon( "bobs_gun_base" )
-    base.Spawnable = false
-    base.AdminSpawnable = true
-    base.ShouldDoMoveSpread = false
-
-    base.OriginalShootBullet = base.ShootBullet
-    base.ShootBullet = function( self, damage, _, numBullets, spread )
-        spread = self.Primary.Spread
-
-        if self.IronSightState then
-            spread = self.Primary.IronAccuracy or spread
-        end
-
-        return self:OriginalShootBullet( damage, _, numBullets, spread )
-    end
-end
-
-stubM9kBase()
-cfcEntityStubber.registerStubber( "cfc_m9k" )
+cfcEntityStubber.registerStubber( "m9k" )
