8335b4005ba9b00cdd57f0bd306eea0bea7ae890
GenericRicochetPlayer <113946528+GenericRicochetPlayer@users.noreply.github.com>
2023-11-03 22:01:31 -0400

    Add files via upload (#55)

--- lua/cfc_entity_stubber/cw/cw_g4p_glock17.lua
+++ lua/cfc_entity_stubber/cw/cw_g4p_glock17.lua
@@ -4,4 +4,7 @@ cfcEntityStubber.registerStub( function()
     local weapon = cfcEntityStubber.getWeapon( "cw_g4p_glock17" )
     weapon.DeployTime = 0
     weapon.HolsterTime = 0
+    weapon.ReloadSpeed = 1.4
+    weapon.Damage = 25
+    weapon.VelocitySensitivity = 0.9
 end )
---
+++ lua/cfc_entity_stubber/cw/cw_l85a2.lua
@@ -0,0 +1,6 @@
+AddCSLuaFile()
+
+cfcEntityStubber.registerStub( function()
+    local weapon = cfcEntityStubber.getWeapon( "cw_l85a2" )
+    weapon.ReloadSpeed = 1.8
+end )
