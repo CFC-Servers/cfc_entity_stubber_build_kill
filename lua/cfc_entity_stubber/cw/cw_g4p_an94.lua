b29a1a3a4223aaf609f2ead8384fdc40acf2a2a4
GenericRicochetPlayer <113946528+GenericRicochetPlayer@users.noreply.github.com>
2023-11-06 10:41:02 -0500

    Add files via upload

---
+++ lua/cfc_entity_stubber/cw/cw_g4p_an94.lua
@@ -0,0 +1,7 @@
+AddCSLuaFile()
+
+cfcEntityStubber.registerStub( function()
+    local weapon = cfcEntityStubber.getWeapon( "cw_g4p_an94" )
+    weapon.ReloadSpeed = 1.4
+    weapon.VelocitySensitivity = 1.4
+end )
