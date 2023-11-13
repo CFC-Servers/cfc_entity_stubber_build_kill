e88767fe22a21ac7fb794fcc357d8bf5708403ae
GenericRicochetPlayer <113946528+GenericRicochetPlayer@users.noreply.github.com>
2023-11-13 18:49:18 -0500

    Add files via upload

--- lua/cfc_entity_stubber/cw/cw_mp7_official.lua
+++ lua/cfc_entity_stubber/cw/cw_mp7_official.lua
@@ -6,4 +6,12 @@ cfcEntityStubber.registerStub( function()
     weapon.HolsterTime = 0
     weapon.ReloadSpeed = 1.7
     weapon.VelocitySensitivity = 0.5
+    weapon.AimSpread = 0.04
+    weapon.HipSpread = 0.05
+    weapon.DrawCrosshair = true
+    weapon.ViewModelMovementScale = 0
+    weapon.ViewbobEnabled = false
+    weapon.AimSwayIntensity = 0
+    weapon.LuaViewmodelRecoil = false
+    weapon.LuaViewmodelRecoilOverride = false
 end )
