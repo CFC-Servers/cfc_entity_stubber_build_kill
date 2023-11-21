ef3285abc49e4b0644d6fe3c8a305724924567cc
GenericRicochetPlayer <113946528+GenericRicochetPlayer@users.noreply.github.com>
2023-11-21 14:58:57 -0500

    Add files via upload

--- lua/cfc_entity_stubber/cw/cw_g4p_masada_acr.lua
+++ lua/cfc_entity_stubber/cw/cw_g4p_masada_acr.lua
@@ -2,5 +2,6 @@ AddCSLuaFile()
 
 cfcEntityStubber.registerStub( function()
     local weapon = cfcEntityStubber.getWeapon( "cw_g4p_masada_acr" )
-    weapon.VelocitySensitivity = 0.5
+    weapon.VelocitySensitivity = 0
+    weapon.Damage = 27
 end )
