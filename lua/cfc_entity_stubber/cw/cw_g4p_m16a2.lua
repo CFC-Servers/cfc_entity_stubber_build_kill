b456a711e84c183a484ebb37febf7abbb4123609
GenericRicochetPlayer <113946528+GenericRicochetPlayer@users.noreply.github.com>
2023-12-16 15:27:03 -0500

    Add files via upload

--- lua/cfc_entity_stubber/cw/cw_g4p_m16a2.lua
+++ lua/cfc_entity_stubber/cw/cw_g4p_m16a2.lua
@@ -4,4 +4,5 @@ cfcEntityStubber.registerStub( function()
     local weapon = cfcEntityStubber.getWeapon( "cw_g4p_m16a2" )
     weapon.DeployTime = 0
     weapon.HolsterTime = 0
+    weapon.Damage = 28
 end )
