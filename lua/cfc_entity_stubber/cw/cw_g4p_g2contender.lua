fc162a446797d52ab6f0201b3480bf27b0c1b5e0
GenericRicochetPlayer <113946528+GenericRicochetPlayer@users.noreply.github.com>
2023-12-17 10:17:28 -0500

    Add files via upload

--- lua/cfc_entity_stubber/cw/cw_g4p_g2contender.lua
+++ lua/cfc_entity_stubber/cw/cw_g4p_g2contender.lua
@@ -4,13 +4,13 @@ cfcEntityStubber.registerStub( function()
     local weapon = cfcEntityStubber.getWeapon( "cw_g4p_g2contender" )
     weapon.Attachments = {}
     weapon.Recoil = 60
-    weapon.Shots = 20
+    weapon.Shots = 18
     weapon.AimSpread = 0.06
     weapon.HipSpread = 0.2
     weapon.SpreadPerShot = 0.01
     weapon.ClumpSpread = 0.1
-    weapon.ReloadSpeed = 0.6
-    weapon.Damage = 20
+    weapon.ReloadSpeed = 0.5
+    weapon.VelocitySensitivity = 2.5
 
     local IsValid = IsValid
 
@@ -27,7 +27,7 @@ cfcEntityStubber.registerStub( function()
         if IsValid( ent ) and ent:IsPlayer() then
             dmg:SetDamageForce( dmg:GetDamageForce() * 100 )
         else
-            dmg:SetDamageForce( dmg:GetDamageForce() * 100 )
+            dmg:SetDamageForce( dmg:GetDamageForce() * 20 )
         end
     end )
 end )
