b4872f72b934b520fb40bb88a72b4a670447a9f0
TankNut <m.vlasblom@live.nl>
2024-07-05 18:09:01 +0200

    Fix simple_ent_hl2e_20mm

--- lua/cfc_entity_stubber/simple_weapons/simple_ent_hl2e_20mm.lua
+++ lua/cfc_entity_stubber/simple_weapons/simple_ent_hl2e_20mm.lua
@@ -3,7 +3,7 @@ AddCSLuaFile()
 -- https://github.com/TankNut/simple-weapons-hl2e/blob/master/lua/entities/simple_ent_hl2e_20mm.lua
 
 cfcEntityStubber.registerStub( function()
-    local entity = cfcEntityStubber.getWeapon( "simple_ent_hl2e_20mm" )
+    local entity = scripted_ents.GetStored( "simple_ent_hl2e_20mm" )
 
     entity.Damage = 60
 end )
