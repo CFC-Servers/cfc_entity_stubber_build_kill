bf6aec0630bbcfd3f5106aeb15f00df89cdf120f
GenericRicochetPlayer <113946528+GenericRicochetPlayer@users.noreply.github.com>
2024-08-04 17:32:29 -0400

    Add files via upload

--- lua/cfc_entity_stubber/cw/cw_ar15.lua
+++ lua/cfc_entity_stubber/cw/cw_ar15.lua
@@ -4,4 +4,5 @@ cfcEntityStubber.registerStub( function()
     local weapon = cfcEntityStubber.getWeapon( "cw_ar15" )
     weapon.DeployTime = 0
     weapon.HolsterTime = 0
+    weapon.Recoil = 0.85
 end )
