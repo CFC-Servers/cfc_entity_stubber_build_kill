aaac1d3ca912700e86eb3d96427ef864373f134c
ProfessorBoxtrot <ProfessorBoxtrot@gmail.com>
2024-12-29 22:17:42 -0800

    Fix typos

--- lua/cfc_entity_stubber/cw/cw_shorty.lua
+++ lua/cfc_entity_stubber/cw/cw_shorty.lua
@@ -4,7 +4,7 @@ cfcEntityStubber.registerStub( function()
     local weapon = cfcEntityStubber.getWeapon( "cw_shorty" )
     weapon.VelocitySensitivity = 0
     weapon.Recoil = 6
-    weapon.damage = 12
+    weapon.Damage = 12
     weapon.HipSpread = 0.06
     weapon.AimSpread = 0.03
     weapon.DeployTime = 0
