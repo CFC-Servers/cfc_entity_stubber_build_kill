ea90ba9fca9a0143188e9b772173868e107bfcd1
ProfessorBoxtrot <ProfessorBoxtrot@gmail.com>
2025-03-11 00:25:17 -0700

    Add TFA Stubber

---
+++ lua/cfc_entity_stubber/tfa/tfa_l4d2mw_shovel.lua
@@ -0,0 +1,6 @@
+AddCSLuaFile()
+
+cfcEntityStubber.registerStub( function()
+    local weapon = cfcEntityStubber.getWeapon( "tfa_l4d2mw_shovel" )
+    weapon.PropDamageMultiplier = 2.5
+end )
---
+++ lua/cfc_entity_stubber/tfa/tfa_l4d2mw_sledgehammer.lua
@@ -0,0 +1,6 @@
+AddCSLuaFile()
+
+cfcEntityStubber.registerStub( function()
+    local weapon = cfcEntityStubber.getWeapon( "tfa_l4d2mw_sledgehammer" )
+    weapon.PropDamageMultiplier = 1.5
+end )
