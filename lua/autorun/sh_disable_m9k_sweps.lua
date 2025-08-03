24023fff63a2c732ddae0cd2d38712eed212fa6d
Pierce Lally <pierce@vulpes.dev>
2025-08-03 00:16:19 -0400

    allow cool shit (#81)
    * allow cool shit
    
    * remove from spawn menu
    
    * remove bad entities

--- lua/autorun/sh_disable_m9k_sweps.lua
+++ lua/autorun/sh_disable_m9k_sweps.lua
@@ -1,4 +1,3 @@
-
 do
     local badClasses = {
         m9k_davy_crockett = true,
@@ -13,27 +12,17 @@ do
         m9k_rpg7 = true,
     }
 
-    hook.Add( "PreRegisterSWEP", "CFC_EntityStubber_DisableM9kSWEPS", function( _, class )
-        if badClasses[class] then return false end
+    hook.Add( "PreRegisterSWEP", "CFC_EntityStubber_DisableM9kSWEPS", function( swep, class )
+        if badClasses[class] then
+            swep.AdminOnly = true
+            swep.Spawnable = false
+        end
     end )
 end
 
 -- Disable bad sents
 do
     local badClasses = {
-        m9k_davy_crockett_explo = true,
-        m9k_gdcwa_matador_90mm = true,
-        m9k_gdcwa_rpg_heat = true,
-        m9k_launched_davycrockett = true,
-        m9k_launched_ex41 = true,
-        m9k_launched_m79 = true,
-        m9k_m202_rocket = true,
-        m9k_nervegasnade = true,
-        m9k_orbital_cannon = true,
-        m9k_poison_parent = true,
-        m9k_released_poison = true,
-        m9k_sent_nuke_radiation = true,
-
         -- Ammo
         m9k_ammo_40mm = true,
         m9k_ammo_c4 = true,
