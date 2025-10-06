8f7ed1e825ea490e5b28749b09cf0739ffc47b5a
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2025-10-06 15:33:17 -0500

    Nerf nitro

---
+++ lua/cfc_entity_stubber/m9k/m9k_nitro.lua
@@ -0,0 +1,18 @@
+AddCSLuaFile()
+
+-- https://github.com/CFC-Servers/m9k_monorepo/blob/main/lua/weapons/m9k_nitro.lua
+cfcEntityStubber.registerStub( function()
+    local weapon = cfcEntityStubber.getWeapon( "m9k_nitro" )
+
+    weapon.Primary.RPM = 20 -- 30
+end )
+
+-- https://github.com/CFC-Servers/m9k_monorepo/blob/main/lua/entities/m9k_nitro_vapor.lua
+cfcEntityStubber.registerStub( function()
+    local entTbl = scripted_ents.GetStored( "m9k_nitro_vapor" )
+    if not entTbl then return end
+
+    entTbl.ExplosionDamage = 600 -- 600
+    entTbl.ExplosionRadius = 150 -- 200
+    entTbl.PropDamageMultiplier = 1.3 -- 1
+end )
