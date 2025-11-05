7bc1855aa9a3be7d479b4ce61606d0d0de0f7469
pierce <pierce@vulpes.dev>
2025-11-05 13:37:17 -0500

    update RPM

--- lua/cfc_entity_stubber/m9k/m9k_nitro.lua
+++ lua/cfc_entity_stubber/m9k/m9k_nitro.lua
@@ -2,5 +2,5 @@ AddCSLuaFile()
 
 cfcEntityStubber.registerStub( function()
     local weapon = cfcEntityStubber.getWeapon( "m9k_nitro" )
-    weapon.Primary.RPM = 17
+    weapon.Primary.RPM = 37
 end )
