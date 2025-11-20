5edc6c2afddac7e16e0d5fb58fb6439f44c19146
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2025-11-20 10:55:39 -0600

    Style

--- lua/cfc_entity_stubber/simple_weapons/simple_hl2e_oicw.lua
+++ lua/cfc_entity_stubber/simple_weapons/simple_hl2e_oicw.lua
@@ -8,6 +8,6 @@ cfcEntityStubber.registerStub( function()
     weapon.Primary.Damage = 26
     weapon.Primary.Range = 1400
 
-    weapon.Primary.Recoil.MinAng = Angle(0.8, -0.4, 0)
-    weapon.Primary.Recoil.MaxAng = Angle(1, 0.4, 0)
+    weapon.Primary.Recoil.MinAng = Angle( 0.8, -0.4, 0 )
+    weapon.Primary.Recoil.MaxAng = Angle( 1, 0.4, 0 )
 end )
