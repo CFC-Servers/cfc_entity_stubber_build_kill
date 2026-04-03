187f934664a491314efe9e3830cb45825af2da29
pierce <pierce@vulpes.dev>
2026-04-03 12:45:42 -0400

    fix

--- lua/autorun/sh_remove_hl2_simple_weapons.lua
+++ lua/autorun/sh_remove_hl2_simple_weapons.lua
@@ -1,4 +1,4 @@
-hook.Add( "PreRegisterSWEP", "CFC_EntityStubber_DisableM9kSWEPS", function( swep, class )
+hook.Add( "PreRegisterSWEP", "CFC_EntityStubber_DisableSimpleWeapons", function( swep, class )
     if swep and swep.Category == "Simple Weapons: Half-Life 2" then
         swep.AdminOnly = true
         swep.Spawnable = false
