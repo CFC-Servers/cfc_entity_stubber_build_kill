5a936a1e8803b52c58a264f959af29323005ccb2
pierce <pierce@vulpes.dev>
2026-04-10 15:47:55 -0400

    apply to m9k category only

--- lua/autorun/sh_running_pistols.lua
+++ lua/autorun/sh_running_pistols.lua
@@ -1,3 +1,5 @@
-hook.Add( "PreRegisterSWEP", "CFC_EntityStubber_AllowRunning", function( swep, class )
-    swep.CanShootWhileRunning = true
+hook.Add( "PreRegisterSWEP", "CFC_EntityStubber_AllowRunning", function( swep, _ )
+    if swep and swep.Category == "M9k Pistols" then
+        swep.CanShootWhileRunning = true
+    end
 end )
