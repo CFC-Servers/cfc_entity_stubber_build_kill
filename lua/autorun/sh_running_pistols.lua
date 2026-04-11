79471ec6e86f9bf7184ee40ad7d0676bd765b2bb
pierce <pierce@vulpes.dev>
2026-04-11 14:00:29 -0400

    k -> K

--- lua/autorun/sh_running_pistols.lua
+++ lua/autorun/sh_running_pistols.lua
@@ -1,5 +1,5 @@
 hook.Add( "PreRegisterSWEP", "CFC_EntityStubber_AllowRunning", function( swep, _ )
-    if swep and swep.Category == "M9k Pistols" then
+    if swep and swep.Category == "M9K Pistols" then
         swep.CanShootWhileRunning = true
     end
 end )
